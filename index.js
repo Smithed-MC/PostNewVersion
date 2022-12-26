68bd7e47e78136f9e575c5a640192545d057e85e
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-12-26 11:44:41 -0500

    added verbosity

--- index.js
+++ index.js
@@ -14,15 +14,18 @@ async function run() {
         const dependencies = core.getMultilineInput('dependencies');
         const token = core.getInput('token', { required: true });
 
+        const downloads = {
+            datapack: datapack
+        }
+        if(resourcepack !== undefined && resourcepack !== '')
+            downloads['resourcepack'] = resourcepack
+
         const response = await fetch(`https://api.smithed.dev/addUserPackVersion?uid=${uid}&pack=${pack}&version=${version}&token=${token}`, {
             method: 'POST', headers: { "Content-Type": "application/json" }, body: JSON.stringify({
                 data: {
                     name: version,
                     breaking: true,
-                    downloads: {
-                        datapack: datapack,
-                        resourcepack: resourcepack ?? undefined
-                    },
+                    downloads: downloads,
                     supports: [supports],
                     dependencies: [dependencies !== undefined ? dependencies.map(d => { const [pack, version] = d.split('@'); return { id: pack, version: version }; }) : []]
                 }
