6f1aea2477c6648c921f8a8915663efff0499d03
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-12-26 11:31:32 -0500

    added verbosity

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import fetch from 'node-fetch'
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('node-fetch')
+
 async function run() {
     try {
         // `who-to-greet` input defined in action metadata file
